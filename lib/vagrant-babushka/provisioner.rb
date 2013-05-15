da06d86f3d377050e22187ce7793cc6374041646
Vladimir Valgis <vladimir.valgis@gmail.com>
2013-05-15 05:01:19 +0400

    Add installing babushka on guest

--- lib/vagrant-babushka/provisioner.rb
+++ lib/vagrant-babushka/provisioner.rb
@@ -7,9 +7,36 @@ module VagrantPlugins
       end
 
       def configure(root_config)
+        @hostname = root_config.vm.hostname
       end
 
       def provision
+        bootstrap_babushka! unless @machine.communicate.test('babushka --version')
+ 
+        commands = @config.deps.map do |(dep_name, dep_args)|
+          args = dep_args.to_a.map { |k, v| "#{k}='#{v}'" }.join(' ')
+          "babushka --update --defaults --colour '#{dep_name}' #{args}"
+        end
+        commands.each do |command|
+          @machine.communicate.sudo(command) do |type, data|
+            color = type == :stdout ? :green : :red
+            @machine.env.ui.info(data.chomp, :color => color, :prefix => false)
+          end
+        end
+      end
+
+      private
+
+      def bootstrap_babushka!
+        require 'net/http'
+        @machine.env.ui.info("Installing babushka on #{@hostname}.")
+        local_tmpfile = remote_tmpfile = "/tmp/babushka_me_up"
+        File.open(local_tmpfile, 'w') {|f| f.write `curl https://babushka.me/up` }
+        @machine.communicate.upload(local_tmpfile, remote_tmpfile)
+        @machine.communicate.sudo("bash #{remote_tmpfile}") do |type, data|
+          color = type == :stdout ? :green : :red
+          @machine.env.ui.info(data.chomp, :color => color, :prefix => false)
+        end
       end
 
     end
