ae5f3662e47412c29e011f8811764f76466ba40b
Vladimir Valgis <vladimir.valgis@gmail.com>
2013-05-16 04:46:53 +0400

    Add config methods for remote deps and local deps; Add synchronized folder for local deps

--- lib/vagrant-babushka/config.rb
+++ lib/vagrant-babushka/config.rb
@@ -1,18 +1,25 @@
 module VagrantPlugins
   module Babushka
     class Config < Vagrant.plugin("2", :config)
-      attr_accessor :args, :deps
+      attr_accessor :args, :deps, :local_deps_path
 
       def initialize
-        @deps = UNSET_VALUE
+        super
+        @deps = []
+        @local_deps_path = UNSET_VALUE
       end
 
       def finalize!
         @deps = [] if @deps == UNSET_VALUE
+        @local_deps_path  = nil if @local_deps_path  == UNSET_VALUE
       end
 
-      def dep(dep_spec, args = {})
-        deps << [dep_spec, args]
+      def local_dep(dep_name, args = {})
+        @deps << ['', dep_name, args]
+      end
+
+      def remote_dep(source, dep_name, args = {})
+        @deps << ["#{source}:", dep_name, args]
       end
     end
   end
--- lib/vagrant-babushka/provisioner.rb
+++ lib/vagrant-babushka/provisioner.rb
@@ -8,20 +8,19 @@ module VagrantPlugins
 
       def configure(root_config)
         @hostname = root_config.vm.hostname
+        if @config.local_deps_path
+          local_path = @config.local_deps_path
+          remote_path = '/home/vagrant/babushka-deps'
+          opts = {id: 'babushka_deps', nfs: false}
+          root_config.vm.synced_folder local_path, remote_path, opts
+        end
       end
 
       def provision
         bootstrap_babushka! unless @machine.communicate.test('babushka --version')
- 
-        commands = @config.deps.map do |(dep_name, dep_args)|
+        @config.deps.map do |(dep_source, dep_name, dep_args)|
           args = dep_args.to_a.map { |k, v| "#{k}='#{v}'" }.join(' ')
-          "babushka --update --defaults --colour '#{dep_name}' #{args}"
-        end
-        commands.each do |command|
-          @machine.communicate.sudo(command) do |type, data|
-            color = type == :stdout ? :green : :red
-            @machine.env.ui.info(data.chomp, :color => color, :prefix => false)
-          end
+          run_remote "babushka --update --defaults --colour #{dep_source}'#{dep_name}' #{args}"
         end
       end
 
@@ -33,7 +32,11 @@ module VagrantPlugins
         local_tmpfile = remote_tmpfile = "/tmp/babushka_me_up"
         File.open(local_tmpfile, 'w') {|f| f.write `curl https://babushka.me/up` }
         @machine.communicate.upload(local_tmpfile, remote_tmpfile)
-        @machine.communicate.sudo("bash #{remote_tmpfile}") do |type, data|
+        run_remote "bash #{remote_tmpfile}"
+      end
+
+      def run_remote(command)
+        @machine.communicate.sudo(command) do |type, data|
           color = type == :stdout ? :green : :red
           @machine.env.ui.info(data.chomp, :color => color, :prefix => false)
         end
