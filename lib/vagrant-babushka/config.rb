57235f29bd9d52df55b12af083535ac2568033b2
Laurent Vallar <val@zbla.net>
2013-10-22 15:48:17 +0200

    Add Babushka bootstrap branch configuration setting
    Signed-off-by: Laurent Vallar <val@zbla.net>

--- lib/vagrant-babushka/config.rb
+++ lib/vagrant-babushka/config.rb
@@ -1,17 +1,19 @@
 module VagrantPlugins
   module Babushka
     class Config < Vagrant.plugin("2", :config)
-      attr_accessor :args, :deps, :local_deps_path
+      attr_accessor :args, :deps, :local_deps_path, :bootstrap_branch
 
       def initialize
         super
         @deps = []
         @local_deps_path = UNSET_VALUE
+        @bootstrap_branch = UNSET_VALUE
       end
 
       def finalize!
         @deps = [] if @deps == UNSET_VALUE
         @local_deps_path  = nil if @local_deps_path  == UNSET_VALUE
+        @bootstrap_branch = nil if @bootstrap_branch == UNSET_VALUE
       end
 
       def local_dep(dep_name, args = {})
