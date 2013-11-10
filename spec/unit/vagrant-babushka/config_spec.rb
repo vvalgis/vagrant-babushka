2ba53d4505c0cb82d2949b1670b751368255fd7d
Brad Feehan <git@bradfeehan.com>
2013-11-11 02:18:33 +1100

    Add unit spec for Config

---
+++ spec/unit/vagrant-babushka/config_spec.rb
@@ -0,0 +1,51 @@
+require "spec_helper"
+
+describe VagrantPlugins::Babushka::Config do
+  let(:unset_value) { described_class::UNSET_VALUE }
+  let(:config) { described_class.new }
+
+  context "with default configuration" do
+    before { subject.finalize! }
+    its(:deps) { should eq [] }
+    its(:local_deps_path) { should be_nil }
+    its(:bootstrap_branch) { should be_nil }
+  end
+
+  describe "#local_deps_path" do
+    before { subject.local_deps_path = '.deps' }
+    its(:local_deps_path) { should eq '.deps' }
+  end
+
+  describe "#bootstrap_branch" do
+    before { subject.bootstrap_branch = 'new' }
+    its(:bootstrap_branch) { should eq 'new' }
+  end
+
+  describe "#local_dep" do
+    before do
+      subject.local_dep 'foobar', :baz => :qux
+      subject.local_dep 'testme', :one => :two
+    end
+
+    it "should store the deps correctly" do
+      expect(subject.deps).to eq [
+        ['', 'foobar', {:baz => :qux}],
+        ['', 'testme', {:one => :two}],
+      ]
+    end
+  end
+
+  describe "#remote_dep" do
+    before do
+      subject.remote_dep 'user1', 'foobar', :baz => :qux
+      subject.remote_dep 'user2', 'testme', :one => :two
+    end
+
+    it "should store the deps correctly" do
+      expect(subject.deps).to eq [
+        ['user1:', 'foobar', {:baz => :qux}],
+        ['user2:', 'testme', {:one => :two}],
+      ]
+    end
+  end
+end
--- vagrant-babushka.gemspec
+++ vagrant-babushka.gemspec
@@ -22,4 +22,5 @@ Gem::Specification.new do |gem|
   gem.add_development_dependency "bundler", "~> 1.3"
   gem.add_development_dependency "rake"
   gem.add_development_dependency "rspec"
+  gem.add_development_dependency "its"
 end
